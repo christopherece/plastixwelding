50ebe358f2aa89c2f8d4c980bbafc91e25f6265a
christopherece <christopheranchetaece@gmail.com>
2025-04-29 23:03:50 +1200

    Modified Colors

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import GalleryImage
+
+@admin.register(GalleryImage)
+class GalleryImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'uploaded_at')
+    search_fields = ('title',)
+    ordering = ('-uploaded_at',)
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import GalleryImage
 
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def gallery(request):
+    images = GalleryImage.objects.all().order_by('-uploaded_at')
+    return render(request, 'pages/gallery.html',{'images': images}) 
