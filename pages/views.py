defb068234076afc24fb5314f1e0a22050703404
christopherece <christopheranchetaece@gmail.com>
2025-05-08 23:11:02 +1200

    Modified Settings

--- pages/views.py
+++ pages/views.py
@@ -6,6 +6,10 @@ from django.contrib import messages
 from .models import ContactMessage
 from .forms import ContactMessageForm
 
+from django.core.mail import send_mail
+from datetime import datetime
+
+
 
 
 
@@ -27,11 +31,39 @@ def gallery(request):
     images = GalleryImage.objects.all().order_by('-uploaded_at')
     return render(request, 'pages/gallery.html',{'images': images}) 
 
+
 def contact_view(request):
     if request.method == 'POST':
         form = ContactMessageForm(request.POST)
         if form.is_valid():
-            form.save()
+            full_name = form.cleaned_data['full_name']
+            email = form.cleaned_data['email']
+            phone_no = form.cleaned_data['phone_no']
+            message = form.cleaned_data['message']
+            
+            form.save()  # Save to DB
+
+            # Construct the email body with all fields
+            email_body = f'''
+You have a new inquiry from your website:
+
+Name: {full_name}
+Email: {email}
+Phone: {phone_no}
+
+Message:
+{message}
+            '''
+
+            # Send the email using the default from address in settings
+            send_mail(
+                'You Have a Notification',
+                email_body,
+                None,  # Uses EMAIL_HOST_USER ('plastixwelding@gmail.com')
+                ['hpwrepair@gmail.com', 'plastixwelding@gmail.com'],
+                fail_silently=False
+            )
+
             messages.success(request, "Your message has been submitted successfully!")
             return redirect('contact_view')
         else:
@@ -39,4 +71,4 @@ def contact_view(request):
     else:
         form = ContactMessageForm()
 
-    return render(request, 'pages/index.html', {'form':form})
+    return render(request, 'pages/index.html', {'form': form})
--- plastixwelding/settings.py
+++ plastixwelding/settings.py
@@ -133,5 +133,12 @@ MESSAGE_TAGS = {
 
 }
 
+#Email Config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'plastixwelding@gmail.com'
+EMAIL_HOST_PASSWORD = 'mrhrzalsdrntjyxt'
+EMAIL_USE_TLS = True
+
 
 
